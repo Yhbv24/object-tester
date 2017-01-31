4fe32b8593bc7988e0d290f0ed28177b67b9f278
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-01-31 15:47:38 -0800

    Add working append to screen. When a user clicks a name that has been added, it will alert that users information

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,18 @@
 //*****FRONT-END*****
 
-function User(firstname, lastname, age, color, live) {
-  this.firstName = firstname;
-  this.lastName = lastname;
+function User(first, last, age, color, live) {
+  this.firstName = first;
+  this.lastName = last;
   this.age = age;
   this.color= color;
   this.live = live;
 };
 
-User.prototype.getFirstLast = function(userFirstName, userLastName) {
+User.prototype.getFirstLast = function() {
   return this.firstName + " " + this.lastName;
 };
 
 //*****FRONT-END*****
-
-
-
 
 $(function() {
   $("#form").submit(function(event) {
@@ -24,8 +21,14 @@ $(function() {
     var userFirstName = $("input[name=first-name]").val();
     var userLastName = $("input[name=last-name]").val();
     var userAge = parseInt($("input[name=age]").val());
-    var userColor = $("select[name=color]:checked").val();
-    var userLive = $("input[type=radio][name=live]:selected").val();
+    var userColor = $("#color").val();
+    var userLive = $("input[name=live]:checked").val();
     var newUser = new User(userFirstName, userLastName, userAge, userColor, userLive);
+
+    $("#output").append("<p>" + newUser.getFirstLast() + "</p><br>");
+
+    $("#output p").last().click(function() {
+      alert("This user's name is " + newUser.getFirstLast() + ", his/her age is " + newUser.age + ", his/her favorite color is " + newUser.color + ", and does he/she live in Portland? " + newUser.live + ".");
+    });
   });
 });
